728333633212ed5df049abd3cdaa1418706058fa
WORK\Work <skheawkhem@gmail.com>
2017-09-19 22:13:45 +0700

    create from login

--- backend/views/Login/index.php
+++ backend/views/Login/index.php
@@ -10,4 +10,51 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+
+      <form class="form-horizontal" role="form">
+          <center><h1> <label>เข้าสู่ระบบ</label></h1></center>
+  <div class="form-group">
+    <label for="input_username" class="col-sm-4 control-label">ชื่อผู้ใช้</label>
+    <div class="col-sm-5">
+      <input type="text" class="form-control" id="input_username" placeholder="ชื่อผู้ใช">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-4 control-label">รหัสผ่าน</label>
+    <div class="col-sm-5">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="รหัสผ่าน">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-4 col-sm-10">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-4 col-sm-10">
+      <button type="submit" class="btn btn-default">เข้าสู่ระบบ</button>
+    </div>
+  </div>
+
+</form>
+
+      </div>
+  </body>
+</html>
 
