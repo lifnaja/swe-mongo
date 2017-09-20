687c6153034666e5e5775ec60de9bb5301c909d3
Nakhon41 <jo_sometime@hotmail.com>
2017-09-20 19:03:02 +0700

    Fix First Path

--- backend/config/main.php
+++ backend/config/main.php
@@ -9,6 +9,7 @@ $params = array_merge(
 return [
     'id' => 'app-backend',
 	'name'=>'Basic App',
+	'defaultRoute' => 'hotel/index',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
