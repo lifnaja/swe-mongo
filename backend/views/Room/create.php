fcd4042c974d07af8ba5f9a7e2d8a9a1581f26fe
DELL <DELL@172.16.33.133>
2017-09-21 00:37:42 +0700

    update create & edit

--- backend/views/Room/create.php
+++ backend/views/Room/create.php
@@ -5,36 +5,39 @@ $this->params['breadcrumbs'][] = $this->title;
 
 $BaseUrl = \Yii::getAlias('@web');
 ?>
-<body>
-
 <div class="container">
-<div class="row">
-<div class="panel panel-info">
-<div class="panel-heading"><b>เพิ่มข้อมูลห้องพัก</b></div>
- <div class="panel-body">
-  
-
-<form action="<?=$BaseUrl."/room/save" ?>" method="get">
-
-  <div class="form-group">
-       <label for="inputEmail3" class="control-label col-sm-2">หมายเลขห้อง:</label>
-       <div class="col-sm-2">
-         <input type="text" class="form-control" name="roomID" placeholder="หมายเลขห้อง">
-     </div>
-        
-		<div class="form-group">
-    <label  class="col-sm-2 control-label" >ประเภทห้อง :</label>
-      <div class="col-sm-2">
-        <select name="num" class="form-control" >
-            <option value="0">ประเภทห้อง</option>
-            <option value="1">Single Bedroom</option>
-            <option value="2">Family Bedroom</option>
-            <option value="3">King Size Bedroom</option>
-        </select></div>
-
-	 <div class="form-group">
-       <label for="inputEmail3" class="control-label col-sm-1">ราคา :</label>
-       <div class="col-sm-1">
-         <input type="text" class="form-control" name="roomID" placeholder="ราคา">
-     </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-</form>
+	<div class="row">
+		<div class="panel panel-info">
+			<div class="panel-heading">
+				<b>เพิ่มข้อมูลห้องพัก</b>
+			</div>
+			<div class="panel-body">
+  				<form action="<?=$BaseUrl."/room/save" ?>" method="get">
+					<div class="form-group">
+       					<label for="inputEmail3" class="control-label col-sm-2">หมายเลขห้อง:</label>
+       						<div class="col-sm-2">
+         						<input type="text" class="form-control" name="roomID" placeholder="หมายเลขห้อง">
+     						</div>
+        					<div class="form-group">
+    							<label  class="col-sm-2 control-label" >ประเภทห้อง :</label>
+      								<div class="col-sm-2">
+        								<select name="num" class="form-control" >
+		            						<option value="0">ประเภทห้อง</option>
+		            						<option value="1">Single Bedroom</option>
+		            						<option value="2">Family Bedroom</option>
+		            						<option value="3">King Size Bedroom</option>
+        								</select>
+        							</div>
+									<div class="form-group">
+       									<label for="inputEmail3" class="control-label col-sm-1">ราคา :</label>
+       										<div class="col-sm-1">
+         										<input type="text" class="form-control" name="roomID" placeholder="ราคา">
+     										</div>
+     								</div>
+     						</div>
+     				</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
--- backend/views/Room/edit.php
+++ backend/views/Room/edit.php
@@ -5,11 +5,39 @@ $this->params['breadcrumbs'][] = $this->title;
 
 $BaseUrl = \Yii::getAlias('@web');
 ?>
+<div class="container">
+	<div class="row">
+		<div class="panel panel-info">
+			<div class="panel-heading">
+				<b>เพิ่มข้อมูลห้องพัก</b>
+			</div>
+ 			<div class="panel-body">
+  				<form action="<?=$BaseUrl."/room/save" ?>" method="get">
+					<div class="form-group">
+      					<label for="inputEmail3" class="control-label col-sm-2">หมายเลขห้อง:</label>
+       						<div class="col-sm-2">
+       							<input type="hidden" name="id" value="<?=$model['_id']?>">
+         						<input type="text" class="form-control" name="roomID" value="<?=$model['roomID']?>">
+     						</div>       
+							<div class="form-group">
+    							<label  class="col-sm-2 control-label" >ประเภทห้อง :</label>
+      								<div class="col-sm-2">
+        								<input type="text" name="type" class="form-control" value="<?=$model['type']?>">
+        							</div>
+        					</div>
+							<div class="form-group">
+       							<label for="inputEmail3" class="control-label col-sm-1">ราคา :</label>
+       								<div class="col-sm-1">
+         								<input type="text" class="form-control" name="roomID"  value="<?=$model['price']?>">
+     								</div>
+     								<label for="inputEmail3" class="control-label col-sm-1">บาท</label>
+     						</div>
+     				</div>
+				</form>			
+			</div>
+		</div>
+	</div>
+</div>
 
-<form action="<?=$BaseUrl."/room/save" ?>" method="get">
-	<input type="hidden" name="id" value="<?=$model['_id']?>">
-	หมายเลขห้อง<input type="text" name="roomID"  value="<?=$model['roomID']?>">
-	ประเภทห้อง<input type="text" name="type"  value="<?=$model['type']?>">
-	ราคา<input type="text" name="price"  value="<?=$model['price']?>">
-	<input type="submit" value="บันทึก">
-</form>
+				
+				
