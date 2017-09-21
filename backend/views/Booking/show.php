328e0bc740ec8c37f262ee1f5904fa6398f290eb
NEXT Speed <NEXT Speed@192.168.99.1>
2017-09-21 11:35:15 +0700

    edit booking/show

--- backend/views/Booking/show.php
+++ backend/views/Booking/show.php
@@ -1,9 +1,6 @@
 <?php
-var_dump($rn);
-var_dump($one); 
-var_dump($two);
-var_dump($three);
-exit();
+
+
 $this->title = 'ห้องว่างทั้งหมด';
 $this->params['breadcrumbs'][] = ['label' => 'การจัดการ','url'=> ['index']];
 $this->params['breadcrumbs'][] = $this->title;
@@ -21,22 +18,21 @@ $BaseUrl = \Yii::getAlias('@web');
         <h5><label  class="col-sm-2 control-label" >Family Bedroom</label></h5>
     </div>
     <div class="form-group">
-        <h5><label  class="col-sm-1 control-label" >จำนวนห้องว่าง</label></h5>
+        <h5><label  class="col-sm-2 control-label" >จำนวนห้องว่าง</label></h5>
       <div class="col-sm-2">
-        <input type="text" class="form-control" name="course_name">
+        <input type="text" class="form-control" name="one" value="<?=$one ?>" disabled>
       </div>
       <div >
           <h5><label  class="col-sm-1 control-label" >ต้องการจอง</label></h5>
           <div class="col-sm-1">
             <select name="num" class="form-control">
-                <option value="0"></option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
+            <?php 
+            	for ($i=0;$i<=$one;$i++){
+            		echo "<option value=".$i.">$i</option>";
+            		
+            	}
+            ?>
+               
             </select>
           </div>
       </div>
@@ -45,22 +41,20 @@ $BaseUrl = \Yii::getAlias('@web');
         <h5><label  class="col-sm-2 control-label" >King Size Bedroom</label></h5>
     </div>
     <div class="form-group">
-        <h5><label  class="col-sm-1 control-label" >จำนวนห้องว่าง</label></h5>
+        <h5><label  class="col-sm-2 control-label" >จำนวนห้องว่าง</label></h5>
       <div class="col-sm-2">
-        <input type="text" class="form-control" name="course_name">
+        <input type="text" class="form-control" name="two" value="<?=$two ?>" disabled>
       </div>
       <div >
           <h5><label  class="col-sm-1 control-label" >ต้องการจอง</label></h5>
           <div class="col-sm-1">
             <select name="num" class="form-control">
-                <option value="0"></option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
+                <?php 
+            	for ($i=0;$i<=$two;$i++){
+            		echo "<option value=".$i.">$i</option>";
+            		
+            	}
+            ?>
             </select>
           </div>
       </div>
@@ -69,22 +63,20 @@ $BaseUrl = \Yii::getAlias('@web');
         <h5><label  class="col-sm-2 control-label" >Single Bedroom</label></h5>
     </div>
     <div class="form-group">
-        <h5><label  class="col-sm-1 control-label" >จำนวนห้องว่าง</label></h5>
+        <h5><label  class="col-sm-2 control-label" >จำนวนห้องว่าง</label></h5>
       <div class="col-sm-2">
-        <input type="text" class="form-control" name="course_name">
+        <input type="text" class="form-control" name="three" value="<?=$three ?>" disabled>
       </div>
       <div >
           <h5><label  class="col-sm-1 control-label" >ต้องการจอง</label></h5>
           <div class="col-sm-1">
             <select name="num" class="form-control">
-                <option value="0"></option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
+                <?php 
+            	for ($i=0;$i<=$three;$i++){
+            		echo "<option value=".$i.">$i</option>";
+            		
+            	}
+            ?>
             </select>
           </div>
       </div>
